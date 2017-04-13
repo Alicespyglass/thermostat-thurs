857a8df38e43de4e51af815e40f833eddd3dba7d
AlexJukes <alexanderjukes@gmail.com>
2017-04-13 11:21:08 +0100

    Add failing test for up function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,7 +9,14 @@ describe('Thermostat', function() {
 
   describe( 'DEFAULT-TEMPERATURE', function() {
     it('has a default temperature of 20 degrees', function() {
-      expect(thermostat.DEFAULTTEMPERATURE).toEqual(20);
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('#up', function() {
+    it('increase temperature by 1 degree', function () {
+      thermostat.up;
+      expect(thermostat.temperature).toEqual(21);
     });
   });
 
---
+++ src/thermostat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function Thermostat() {
+
+  var DEFAULTTEMPERATURE = 20
+
+
+  this.temperature = DEFAULTTEMPERATURE
+};
